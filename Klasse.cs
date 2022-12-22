765d05b2ca33b03813c7e86732ba4b83b2860015
Joingoin <Nico.doeller@outlook.de>
2022-12-22 08:47:51 +0100

    UnnoetigeVariablen

--- Klasse.cs
+++ Klasse.cs
@@ -5,16 +5,31 @@ namespace refactoring
 
         
         int anzahlReifen = 0;
+        int anzahlTurbinen = 0;
+        int anzahlSchiffsschrauben = 0;
 
         public int AnzahlReifen
         {
             get{return anzahlReifen;}
         }
 
+        public int anzahlTurbinen
+        {
+            get{return anzahlTurbinen;}
+        }
+
+        public int AnzahlSchiffsschrauben
+        {
+            get{return anzahlSchiffsschrauben;}
+        }
+
         
-        public Transportmittel(int anzahlReifen)
+        public Transportmittel(int anzahlReifen,int anzahlTurbinen,int anzahlSchiffsschrauben)
         {
             this.anzahlReifen = anzahlReifen;
+            this.anzahlTurbinen = anzahlTurbinen;
+            this.anzahlSchiffsschrauben = anzahlSchiffsschrauben;
+
         }
 
 
