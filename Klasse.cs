aba6cf7cd758ebc463039a2c5c0fd3bd6fe96096
Joingoin <Nico.doeller@outlook.de>
2022-12-22 09:13:38 +0100

    UnauffaelligUmlackieren Methode

--- Klasse.cs
+++ Klasse.cs
@@ -11,6 +11,8 @@ namespace refactoring
         int anzahlSchiffsschrauben = 0;
         string kennung = UNKNOWN;
 
+        string farbe = "weiß";
+
 
         public int PS = 0;
         public int ZugelassenesGewicht = 0;
@@ -46,6 +48,11 @@ namespace refactoring
         public string Kennung
         {
             get{return kennung;}
+        }
+
+        public string farbe
+        {
+            get{farbe;}
         }
 
         
@@ -100,6 +107,41 @@ namespace refactoring
 
             if((PS * 10) < g )
                 return true;
+
+        }
+
+
+        public void UnauffaelligUmlackieren(int l)
+        {
+            string[] farben = new string[]{"Rot","Grün","Blau","Gelb","Karmesin","Aufgehende Sonne im Morgenrot","Aquamarin","Weiß","Grau"};
+            string k = "";
+            int l = 10;
+            char[] chars = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+            
+            Random r = new Random();
+            int i = r.Next(0,farben.Length);
+
+
+            farbe = farben[i];
+
+            int i = r.Next(1,11);
+
+
+            while(k.Length < l)
+            {
+                if(i > 8)
+                {
+                    k = k + chars[r.Next(0,chars.Length-1)];
+                }
+                else
+                {
+                    k = k + Convert.ToSting(r.Next(1,10));
+                }
+            }
+
+            kennung = k;
+
 
         }
 
