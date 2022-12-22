36272bc39932cc28a685325ff76032deb8bb0538
Quentin Schneidau <Quentin.s@gso.schule.koeln>
2022-12-22 08:48:40 +0100

    Quentins erster push

--- Klasse.cs
+++ Klasse.cs
@@ -3,16 +3,31 @@ namespace refactoring
     class Transportmittel
     {
 
-        
+        int anzahlTriebwerke = 0;
+        int anzahlFluegel = 0;
+        int anzahlTueren = 0;
         int anzahlReifen = 0;
 
+
+        public int AnzahlTriebwerke
+        {
+            get {return anzahlTriebwerke;}
+        }
+        public int AnzahlFluegel
+        {
+            get {return anzahlFluegel;}
+        }
+        public int AnzahlTueren
+        {
+            get {return anzahlTueren;}
+        }
         public int AnzahlReifen
         {
             get{return anzahlReifen;}
         }
 
         
-        public Transportmittel(int anzahlReifen)
+        public Transportmittel(int anzahlReifen,int anzahlTueren,int anzahlFluegel, int anzahlTriebwerke)
         {
             this.anzahlReifen = anzahlReifen;
         }
