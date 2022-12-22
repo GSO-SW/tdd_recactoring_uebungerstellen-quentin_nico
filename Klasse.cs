9ee5c233c16daf8e326df2d4581a9167b2709157
Joingoin <Nico.doeller@outlook.de>
2022-12-22 08:43:25 +0100

    Erste Variable

--- Klasse.cs
+++ Klasse.cs
@@ -2,10 +2,19 @@ namespace refactoring
 {
     class Transportmittel
     {
+
         
-        public Transportmittel()
+        int anzahlReifen = 0;
+
+        public int AnzahlReifen
         {
-            
+            get{return anzahlReifen;}
+        }
+
+        
+        public Transportmittel(int anzahlReifen)
+        {
+            this.anzahlReifen = anzahlReifen;
         }
 
 
