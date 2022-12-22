217c174bbb9a80456409328d0c7862e5225f0de1
Joingoin <Nico.doeller@outlook.de>
2022-12-22 08:59:50 +0100

    KennungsMethode

--- Klasse.cs
+++ Klasse.cs
@@ -9,6 +9,7 @@ namespace refactoring
         int anzahlReifen = 0;
         int anzahlTurbinen = 0;
         int anzahlSchiffsschrauben = 0;
+        string kennung = UNKNOWN;
 
 
         public int AnzahlTriebwerke
@@ -38,12 +39,49 @@ namespace refactoring
             get{return anzahlSchiffsschrauben;}
         }
 
+        public string Kennung
+        {
+            get{return kennung;}
+        }
+
         
         public Transportmittel(int anzahlReifen,int anzahlTurbinen,int anzahlSchiffsschrauben,int anzahlTueren,int anzahlFluegel, int anzahlTriebwerke)
         {
             this.anzahlReifen = anzahlReifen;
             this.anzahlTurbinen = anzahlTurbinen;
             this.anzahlSchiffsschrauben = anzahlSchiffsschrauben;
+            this.kennung = ZufaelligeKennungVergeben();
+
+        }
+
+
+        
+
+        private string ZufaelligeKennungVergeben()
+        {
+            string k = "";
+            int l = 10;
+            char[] chars = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+            Random r = new Random();
+
+            int i = r.Next(1,11);
+
+
+            while(k.Length < l)
+            {
+                if(i > 8)
+                {
+                    k = k + chars[r.Next(0,chars.Length-1)];
+                }
+                else
+                {
+                    k = k + Convert.ToSting(r.Next(1,10));
+                }
+            }
+
+            return k;
+
 
         }
 
